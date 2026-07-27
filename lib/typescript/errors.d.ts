e88b333467b9581653dbd8011379eb4884acd2d9
XDcobra <xdcobra11@gmail.com>
2026-07-27 12:47:53 +0800

    chore: stop tracking generated lib and add prepare build

--- lib/typescript/errors.d.ts
+++
@@ -1,8 +0,0 @@
-import { GrpcMetadata } from './types';
-export declare class GrpcError extends Error {
-    error: string;
-    code?: number | undefined;
-    trailers?: GrpcMetadata | undefined;
-    constructor(error: string, code?: number | undefined, trailers?: GrpcMetadata | undefined);
-}
-//# sourceMappingURL=errors.d.ts.map
